f762ac3bf9afdc96927f8a5f30399066432e0c6d
Montimare <69976369+Montimare@users.noreply.github.com>
2023-06-07 13:01:20 +0200

    edited leak.cpp

--- ab6/leak.cpp
+++ ab6/leak.cpp
@@ -2,125 +2,230 @@
 #include <string>
 #include <new>
 
-
-
 using namespace std;
 
-class Person {
- 
-public: 
-  Person();
-  void n_ausgabe();
-  
-  
-private:  
-  char* vorname;
-  int vlaenge;
-  char* nachname;
-  int nlaenge;
-  
-  friend class Kunde;
-};
+class Person
+{
 
-
-Person::Person() {
- 
-  string v;
-  string n;
-  cout << "Vorname: ";
-  getline(cin,v);
-  cout << "Nachname: ";
-  getline(cin,n);
-  int i;
-  
-  vlaenge = v.length()+1; 
-  nlaenge = n.length()+1;
-  
-  vorname = new char [vlaenge]; 
-  
-  for(i=0; i<v.length();i++){
-   
-    vorname[i] = v[i];
-    
-  }
-  
-  vorname[i]='\0';
-  
-  nachname = new char [nlaenge];
-  
-  for(i=0; i<n.length();i++){
-   
-    nachname[i] = n[i];
-    
-  }
-  
-  nachname[i]='\0';
-  
-}
-
-
-void Person::n_ausgabe(){
- cout << "Name: " << vorname << " " << nachname << endl; 
-}
-
-
-class Kunde{
-  
-  Person kunde;
- 
-  long kundennummer;
-  
-  
 public:
-  
-  Kunde(long nummer);
-  Kunde(Person neukunde, long nummer);
-  void print();
-  
+    Person();
+    ~Person();
+    Person(const Person &p);
+    void n_ausgabe() const;
+    Person& operator= (const Person& p);
+
+private:
+    char *vorname;
+    int vlaenge;
+    char *nachname;
+    int nlaenge;
+
+    friend class Kunde;
 };
 
-Kunde::Kunde(long nummer) : kundennummer(nummer) {}
-Kunde::Kunde(Person neukunde, long nummer) : kunde(neukunde), kundennummer(nummer) { 
+Person::Person()
+{
+
+    string v;
+    string n;
+    cout << "Vorname: ";
+    getline(cin, v);
+    cout << "Nachname: ";
+    getline(cin, n);
+    int i;
+
+    vlaenge = v.length() + 1;
+    nlaenge = n.length() + 1;
+
+    vorname = new char[vlaenge];
+
+    for (i = 0; i < v.length(); i++)
+    {
+        vorname[i] = v[i];
+    }
+
+    vorname[i] = '\0';
+
+    nachname = new char[nlaenge];
+
+    for (i = 0; i < n.length(); i++)
+    {
+
+        nachname[i] = n[i];
+    }
+
+    nachname[i] = '\0';
+    cout << "Personen Objekt " << vorname << " " << nachname << " erstellt." << endl;
 }
 
-void Kunde::print() {
- 
-  cout << "Die Kundennummer des Kunden " << kunde.vorname << " " << kunde.nachname << " lautet: " << kundennummer << endl;
-  
+Person::~Person()
+{
+    delete[] vorname;
+    delete[] nachname;
+    cout << "Personen Objekt " << vorname << " " << nachname << " zerstört." << endl;
 }
 
-Kunde kunde_anlegen(){
- 
-  long nr;
-  cout << "Neuen Kunden anlegen!" << endl;
-  Person neu;
-  cout << "Bitte geben Sie die Kundennummer ein: ";
-  cin >> nr;
-  cin.ignore();
-  
-  Kunde k{neu,nr};
-  
-  
-  
-  
-  return k;
-  
+Person::Person(const Person &p)
+{
+    vlaenge = p.vlaenge;
+    nlaenge = p.nlaenge;
+
+    vorname = new char[vlaenge];
+    for (int i = 0; i < vlaenge; i++)
+    {
+        vorname[i] = p.vorname[i];
+    }
+    
+    nachname = new char[nlaenge];
+    for (int i = 0; i < nlaenge; i++)
+    {
+        nachname[i] = p.nachname[i];
+    }
+
+    cout << "Personen Objekt " << vorname << " " << nachname << " kopiert." << endl;
 }
 
-int main() {
-  
- 
-  
- Kunde neuer{kunde_anlegen()}; 
- 
- Kunde dagobert{9999};
- 
- neuer.print();
- dagobert.print();
- 
- dagobert=neuer;
- 
- neuer.print();
- dagobert.print();
-  
+void Person::n_ausgabe() const
+{
+    cout << "Name: " << vorname << " " << nachname << endl;
 }
+
+Person& Person::operator= (const Person& p)
+{
+    if (this == &p){
+        return *this;
+    }
+    delete[] vorname;
+    delete[] nachname;
+
+    vlaenge = p.vlaenge;
+    nlaenge = p.nlaenge;
+
+    vorname = new char[vlaenge];
+    for (int i = 0; i < vlaenge; i++)
+    {
+        vorname[i] = p.vorname[i];
+    }
+    
+    nachname = new char[nlaenge];
+    for (int i = 0; i < nlaenge; i++)
+    {
+        nachname[i] = p.nachname[i];
+    }
+
+    cout << "Personen Objekt " << vorname << " " << nachname << " kopiert." << endl;
+
+    //Person(p);
+    return *this;
+}
+
+
+class Kunde
+{
+
+    Person kunde;
+
+    long kundennummer;
+
+public:
+    Kunde(long nummer);
+    Kunde(Person neukunde, long nummer);
+    void print() const;
+};
+
+Kunde::Kunde(long nummer) : kundennummer(nummer) {}                                            // Initialisierungsliste mit leerem Körper (leer, da keine weiteren Attribute) und Initialisierung der Kundennummer
+Kunde::Kunde(const Person neukunde, const long nummer) : kunde(neukunde), kundennummer(nummer) // Initialisierungsliste mit leerem Körper (leer, da keine weiteren Attribute) und Initialisierung der Kundennummer und des Kunden (Person) mit dem übergebenen Objekt neukunde (Kopie) und der Kundennummer nummer (Kopie)
+{
+}
+
+void Kunde::print() const
+{
+
+    cout << "Die Kundennummer des Kunden " << kunde.vorname << " " << kunde.nachname << " lautet: " << kundennummer << endl;
+}
+
+Kunde kunde_anlegen()
+{
+
+    long nr;
+    cout << "Neuen Kunden anlegen!" << endl;
+    Person neu;
+    cout << "Bitte geben Sie die Kundennummer ein: ";
+    cin >> nr;
+    cin.ignore();
+
+    Kunde k{neu, nr};
+
+    return k;
+}
+
+int main()
+{
+
+    Kunde neuer{kunde_anlegen()};
+    Kunde dagobert{9999};
+
+    neuer.print();
+    dagobert.print();
+
+    dagobert = neuer;
+
+    neuer.print();
+    dagobert.print();
+}
+
+
+/////////////////////////////
+//Das Problem in Ihrem Code ist, dass Sie mehrmals Kopien von Objekten erstellen und diese dann löschen. Dies führt dazu, dass der Speicher für einige Objekte mehrmals freigegeben wird, was zu undefiniertem Verhalten und fehlerhafter Ausgabe führt.
+//
+//Um das Problem zu beheben, sollten Sie den Kopierkonstruktor und den Zuweisungsoperator für die Kunde-Klasse verwenden. Da Sie bereits den Kopierkonstruktor und den Zuweisungsoperator für die Person-Klasse definiert haben, können Sie diese auch in der Kunde-Klasse verwenden.
+//
+//Hier ist die geänderte Kunde-Klasse mit dem Kopierkonstruktor und dem Zuweisungsoperator:
+//
+//class Kunde
+//{
+//    Person kunde;
+//    long kundennummer;
+//
+//public:
+//    Kunde(long nummer);
+//    Kunde(const Person& neukunde, long nummer);
+//    Kunde(const Kunde& k); // Kopierkonstruktor
+//    Kunde& operator=(const Kunde& k); // Zuweisungsoperator
+//    void print() const;
+//};
+//
+//Kunde::Kunde(long nummer) : kundennummer(nummer) {}
+//Kunde::Kunde(const Person& neukunde, long nummer) : kunde(neukunde), kundennummer(nummer) {}
+//
+//// Kopierkonstruktor
+//Kunde::Kunde(const Kunde& k) : kunde(k.kunde), kundennummer(k.kundennummer) {}
+//
+//// Zuweisungsoperator
+//Kunde& Kunde::operator=(const Kunde& k)
+//{
+//    if (this == &k) {
+//        return *this;
+//    }
+//
+//    kunde = k.kunde;
+//    kundennummer = k.kundennummer;
+//
+//    return *this;
+//}
+//Dann ändern Sie die Funktion kunde_anlegen(), um eine Referenz auf das Person-Objekt an den Kunde-Konstruktor zu übergeben:
+//
+//Kunde kunde_anlegen()
+//{
+//    long nr;
+//    cout << "Neuen Kunden anlegen!" << endl;
+//    Person neu;
+//    cout << "Bitte geben Sie die Kundennummer ein: ";
+//    cin >> nr;
+//    cin.ignore();
+//
+//    Kunde k{neu, nr};
+//
+//    return k;
+//}
+//Mit diesen Änderungen sollte Ihr Code korrekt funktionieren, ohne unerwünschte Kopien und Speicherfreigaben.
